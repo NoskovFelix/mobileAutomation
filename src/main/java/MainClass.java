8bc858be164f74a054de60823f58fc69a606b2a6
fnoskov <Feliks.Noskov@vsegda-da.com>
2021-10-07 18:19:09 +0300

    Test3

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,9 @@
 public class MainClass {
 
     private static final int class_number = 20;
+    private static final String class_string = "Hello, world";
+
+    public static String getClassString(){ return class_string; }
 
     public static int getClassNumber(){ return class_number; }
 
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -14,8 +14,15 @@ public class MainClassTest {
     @Test
     public void testGetClassNumber(){
         int expectedValue = MainClass.getClassNumber();
-        Assert.assertTrue("метод getClassNumber вернул число < 45, вернулось " + expectedValue,
+        Assert.assertTrue("Метод getClassNumber вернул число < 45, вернулось " + expectedValue,
                 expectedValue > 45);
+    }
+
+    @Test
+    public void testGetClassString(){
+        String expectedStr = MainClass.getClassString();
+        Assert.assertTrue("Строка 'Hello, world' НЕ содержит подстрок 'Hello' или 'hello'",
+                expectedStr.contains("hello") || expectedStr.contains("Hello"));
     }
 
 }
