6cf5d4a5d24d182dd703268186633d5ef0de9401
fnoskov <Feliks.Noskov@vsegda-da.com>
2021-10-23 07:34:28 +0300

    Ex2: Создание метода

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -2,9 +2,16 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -31,8 +38,36 @@ public class FirstTest {
         driver.quit();
     }
 
+
     @Test
-    public void firstTest(){
-        System.out.println("First test run");
+    public void testCheckContainsThatSearchFieldHasText(){
+
+        assertElementHasText(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_container']//*[@text='Search Wikipedia']"),
+                "Search Wikipedia",
+                "text 'Search Wikipedia' not present in the search field"
+        );
     }
+
+    public void assertElementHasText(By by, String expectedText, String errorMessage){
+        WebElement element = waitForElementPresent(by,  "element with text = '" + expectedText + "' not present on the page");
+        String actualText = element.getText();
+        Assert.assertEquals(errorMessage, expectedText, actualText);
+    }
+
+    private WebElement waitForElementPresent(By by, String errorMessage, long timeoutInSec){
+
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeoutInSec);
+        webDriverWait.withMessage(errorMessage + "\n");
+        return webDriverWait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String errorMessage){
+
+        return waitForElementPresent(by, errorMessage, 5);
+    }
+
+
 }
