f0858458ef5aa9f5bc9965a06e21f5eb2c60ccf0
fnoskov <Feliks.Noskov@vsegda-da.com>
2021-10-23 16:21:40 +0300

    Ex3: Тест: отмена поиска

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -49,24 +49,68 @@ public class FirstTest {
         );
     }
 
-    public void assertElementHasText(By by, String expectedText, String errorMessage){
-        WebElement element = waitForElementPresent(by,  "element with text = '" + expectedText + "' not present on the page");
+    @Test
+    public void testCheckThatResultsAreGone(){
+
+        waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/search_container']//*[@text='Search Wikipedia']"));
+
+        waitForElementAndSendKeys(By.id("org.wikipedia:id/search_src_text"), "Java");
+
+        compareThatActualValueMoreThanExpected(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title']"), 1);
+
+        waitForElementAndClick(By.id("org.wikipedia:id/search_close_btn"));
+
+        waitForElementNotPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title']"),
+                "element displayed on the page",
+                5);
+    }
+
+
+
+    private void compareThatActualValueMoreThanExpected(By by, int expectedValue){
+        int actualValue = driver.findElements(by).size();
+        Assert.assertTrue("actual value less than expected", actualValue > expectedValue);
+    }
+
+
+    private void assertElementHasText(By by, String expectedText, String errorMessage){
+        WebElement element = waitForElementPresent(by);
         String actualText = element.getText();
         Assert.assertEquals(errorMessage, expectedText, actualText);
     }
 
-    private WebElement waitForElementPresent(By by, String errorMessage, long timeoutInSec){
+    private WebElement waitForElementPresent(By by, long timeoutInSec){
 
         WebDriverWait webDriverWait = new WebDriverWait(driver, timeoutInSec);
-        webDriverWait.withMessage(errorMessage + "\n");
+        webDriverWait.withMessage("Element '" + by.toString() + "' not present on the page" + "\n");
         return webDriverWait.until(
                 ExpectedConditions.presenceOfElementLocated(by)
         );
     }
 
-    private WebElement waitForElementPresent(By by, String errorMessage){
+    private boolean waitForElementNotPresent(By by, String errorMessage, long timeoutInSec){
 
-        return waitForElementPresent(by, errorMessage, 5);
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeoutInSec);
+        webDriverWait.withMessage("Element '" + by.toString() + "' present on the page" + "\n");
+        return webDriverWait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresent(By by){
+
+        return waitForElementPresent(by, 5);
+    }
+
+    private void waitForElementAndClick(By by){
+        WebElement element = waitForElementPresent(by);
+        element.click();
+    }
+
+    private void waitForElementAndSendKeys(By by, String value){
+        WebElement element = waitForElementPresent(by);
+        element.sendKeys(value);
     }
 
 
